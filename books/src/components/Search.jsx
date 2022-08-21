4904c2e5855b32bcb0efe4f5661ccbb48cdf00e1
Shehab el-deen <shehab.abdullah12@gmail.com>
2022-08-21 23:33:46 +0200

    fetch data from api

--- books/src/components/Search.jsx
+++ books/src/components/Search.jsx
@@ -1,27 +1,62 @@
 import React from "react";
+import BookCard from "./BookCard";
 
 class Search extends React.Component {
     constructor() {
         super();
+        this.state = {
+            searchText: '',
+            results: []
+        }
+
+        this.textChanged = this.textChanged.bind(this);
+        this.timerId = null;
     }
 
     render() {
         return (
             <>
                 <div className="d-flex flex-row py-4">
-                    <div>
-                        <input className="form-control" type="text" name="" id="search-box" />
-                    </div>
-                    <div>
-                        <input className="btn btn-primary mx-3" type="submit" value="Search" />
-                        <button className="btn btn-sm btn-outline-danger" onClick={this.props.cancelSearch}>cancel</button>
-                    </div>
+                    <input placeholder="What are you searching for?" className="form-control" value={this.state.searchText} onChange={this.textChanged} type="text" name="" id="search-box" />
+                    <button className="btn btn-sm btn-outline-danger mx-4" onClick={this.props.cancelSearch}>cancel</button>
                 </div>
                 <div className="search-results">
-
+                    
                 </div>
             </>
         );
+    }
+
+    fetchData(keyword) {
+        fetch(`http://openlibrary.org/search.json?q=${keyword}`)
+            .then((response) => response.json())
+            .then(
+                (data) => {
+                    const books = this.mapResults(data.docs);
+                    this.setState({ results: books });
+                }
+            )
+    }
+
+    mapResults(results) {
+        const booksWithCover = results.filter(results => results.has_fulltext);
+        const ret = booksWithCover.map(
+            result => {
+                return {
+                    title: result.title,
+                    cover: `https://covers.openlibrary.org/b/id/${result.cover_i}.jpg`
+                }
+            }
+        )
+        return ret;
+    }
+    textChanged(event) {
+        this.setState({ searchText: event.target.value });
+        clearTimeout(this.timerId);
+        this.timerId = setTimeout(() => {
+            this.fetchData(this.state.searchText);
+        }, 1000)
+
     }
 }
 
