d605850565a945a3b0a177cc5a1127f2416891a9
Shehab el-deen <shehab.abdullah12@gmail.com>
2022-08-22 15:36:57 +0200

    create more menue

--- books/src/components/BookCard.jsx
+++ books/src/components/BookCard.jsx
@@ -3,23 +3,43 @@ import React from 'react';
 class BookCard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            moreMenue: false
+        }
+        this.showMore = this.showMore.bind(this);
+    }
+
+    showMore() {
+        this.setState({ moreMenue: !this.state.moreMenue });
     }
 
     render() {
-        const {title, cover} = this.props;
+        const { title, cover } = this.props;
         return (
             <div className="col-sm-6 col-xs-12 col-md-4 mb-4">
                 <div className="card book-card">
                     <div className="card-image">
-                        <img className="w-100" src= {cover} alt="" />
+                        <img className="w-100" src={cover} alt="" />
                     </div>
                     <div className="card-body">
                         <div className="book-title">
                             {title}
                         </div>
                     </div>
-                    <div className="action-btn">
-                        M
+                    <div className="more-menue">
+                        {this.state.moreMenue
+                            ?
+                            <div className='menue'>
+                                <button className='btn'>Want to read</button>
+                                <button className='btn'>One</button>
+                                <button className='btn'>One</button>
+                                <button className='btn'>One</button>
+                            </div>
+                            : <></>
+                        }
+                        <div onClick={this.showMore} className="action-btn">
+                            {this.state.moreMenue? 'X':'>>'}
+                        </div>
                     </div>
                 </div>
             </div>
