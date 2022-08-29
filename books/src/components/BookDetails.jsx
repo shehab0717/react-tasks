6b23f5417a126df1bc4360376aa61ea55f1758c5
Shehab el-deen <shehab.abdullah12@gmail.com>
2022-08-29 14:54:03 +0200

    fix book data null issue

--- books/src/components/BookDetails.jsx
+++ books/src/components/BookDetails.jsx
@@ -1,17 +1,15 @@
 
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 export default function BookDetails() {
     const params = useParams();
     const [loading, setLoading] = useState('true');
-
-    var bookData;
+    const [bookData, setBookData] = useState();
     function getBook() {
         fetch(`http://openlibrary.org/works/${params.bookId}.json`)
             .then(response => response.json())
             .then(data => {
-                bookData = data;
-                console.log(data);
+                setBookData(data);
                 setLoading(false);
             });
     }
@@ -23,5 +21,20 @@ export default function BookDetails() {
     }, [])
     return loading
         ? <div className='spinner'></div>
-        : <div>{bookData.title}</div>
+        : <div>
+            <Link className='btn btn-primary mt-5' to='/'>Home</Link>
+            <div className='row mt-2'>
+                <div className="col-12 col-sm-4 col-lg-3">
+                    <img className='w-100' src={`https://covers.openlibrary.org/b/id/${bookData.covers[0]}.jpg`} />
+                </div>
+                <div className="col">
+                    <h1>{bookData.title}</h1>
+                    <p>
+                        {bookData.description
+                            ? bookData.description.value
+                            : 'No description for this book!'}
+                    </p>
+                </div>
+            </div>
+        </div>
 }
