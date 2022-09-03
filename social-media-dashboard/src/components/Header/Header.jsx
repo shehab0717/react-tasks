8b9a2e8d28178aca824a3ee67fbb194dc42bf480
Shehab el-deen <shehab.abdullah12@gmail.com>
2022-09-03 15:17:18 +0200

    theme provider and toggler, change style depending on theme

--- social-media-dashboard/src/components/Header/Header.jsx
+++ social-media-dashboard/src/components/Header/Header.jsx
@@ -1,21 +1,27 @@
+import { useContext } from 'react'
+import {ThemeContext} from '../../Context/ThemeContext'
 import './Header.css'
 export default function Header() {
+
+    const{theme, toggle} = useContext(ThemeContext);
     return (
-        <div className='Header'>
+
+        <div className={`Header ${theme}-text ${theme}-bg`}>
             <div className="container">
                 <div className="row">
                     <div className='d-flex justify-content-between align-items-center py-3'>
                         <div>
                             <h3 className="mb-0">Social media dashboard</h3>
-                            <span className="text-secondary fw-bold">Total followers: 23,004</span>
+                            <span className="text-color fw-bold">Total followers: 23,004</span>
                         </div>
                         <div className='d-flex'>
                             <label htmlFor="">Dark mode</label>
-                            <div className="theme-switch ms-2"></div>
+                            <div onClick={()=>toggle()} className={`theme-switch ${theme}-switch ms-2`}></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+            
     )
 }
--- social-media-dashboard/src/index.js
+++ social-media-dashboard/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from './Context/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <App />
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
 );
 
 reportWebVitals();
